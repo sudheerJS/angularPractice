ed4f9d144eb28e9671e3bb758469bae083b145a9
sudheerJS <chinnabuddoda@gmail.com>
2021-04-01 15:51:51 +0530

    2 files modified

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -6,8 +6,22 @@ import { Component } from "@angular/core";
   styleUrls: ["./employee.component.css"]
 })
 export class EmployeeComponent {
-  employees: any[] = [
+  emplotees: any[];
+
+  employees = [
     { code: "code1", name: "sudheer", gender: "male", dob: "10/08/1983" },
     { code: "code2", name: "Buddoda", gender: "female", dob: "10/10/1987" }
   ];
+
+  getEmployeeCount(): number {
+    return this.employees.length;
+  }
+
+  getMaleEmployeeCount(): number {
+    return this.employees.filter(e => e.gender === "male").length;
+  }
+
+  getFemaleEmployeeCount(): number {
+    return this.employees.filter(e => e.gender === "female").length;
+  }
 }
--- src/app/employeecount/employeecount.component.ts
+++ src/app/employeecount/employeecount.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "employee-count",
@@ -6,7 +6,10 @@ import { Component } from "@angular/core";
   styleUrls: ["./employeecount.component.css"]
 })
 export class EmployeeCountComponent {
-  all: number = 10;
-  male: number = 5;
-  female: number = 5;
+  @Input()
+  all: number;
+  @Input()
+  male: number;
+  @Input()
+  female: number;
 }
